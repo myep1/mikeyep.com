013bdb33fa71f07af2e2168aa9cfe2d751eecf26
Michael Yep <myep01@yahoo.com>
2025-05-28 15:03:59 -0500

    Added my GPG key

--- src/App.jsx
+++ src/App.jsx
@@ -23,12 +23,12 @@ function App() {
                   {renderHackerTyper === 'true' && <HackerTyper />}
                 </div>
                 <div className="footer-block">
-                  <a href="#/2048" target="_self">Play 2048 (Original)</a>
-                  &nbsp;&nbsp;
-                  <a href="#/dashboard" target="_self">Dashboard</a>
-                  &nbsp;&nbsp;
-                  <a href="https://www.linkedin.com/in/mike-yep" target="_blank">Linkedin Profile</a>
-                  <br />
+                  <div className="footer-links">
+                    <a href="#/2048">Play 2048 (Original)</a>
+                    <a href="#/dashboard">Dashboard</a>
+                    <a href="/gpg.txt" target="_blank">GPG Key</a>
+                    <a href="https://www.linkedin.com/in/mike-yep" target="_blank">Linkedin Profile</a>
+                  </div>
                   <span>© {year} Mike Yep. All Rights Reserved.</span>
                   <p>{deployType}{deployTimestamp}</p>
                 </div>
